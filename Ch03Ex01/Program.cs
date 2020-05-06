5543c1378206ea1f1ad9d8e5b1fb6a1d91efc29e
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-06 17:46:46 +0200

    Ch03Ex01+02 and Exer3.5

---
+++ Ch03Ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BeginningCSharp7
+{
+    /// <summary>
+    /// Using Simple Type Variables
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+            Console.ReadKey();
+        }
+    }
+}
