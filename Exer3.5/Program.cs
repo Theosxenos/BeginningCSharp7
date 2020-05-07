3334ac4e6db36d35c47cd9415d7ca0ede9374b0d
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-07 18:46:17 +0200

    3.5 init

--- Exer3.5/Program.cs
+++ Exer3.5/Program.cs
@@ -1,29 +1,12 @@
 ﻿using System;
-using static System.Console;
 
-namespace Exer3._4
+namespace Exer3._5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double resultVar = 0;
-            double var1 = 1;
-            double var2 = 5;
-            double var3 = 2;
-            double var4 = 3;
-            double var5 = 4;
-
-            // The order in which the operators take precedence
-            double stepOne = var1 * var2;
-            double stepTwo = var4 / var5;
-            double stepThree = var3 % stepTwo;
-            double stepFour = stepOne + stepThree;
-            double manualResult = resultVar + stepFour;
-            WriteLine($"Manual result: {manualResult}");
-
-            resultVar += var1 * var2 + var3 % var4 / var5;
-            WriteLine($"Result: {resultVar}");
+            Console.WriteLine("Hello World!");
         }
     }
 }
