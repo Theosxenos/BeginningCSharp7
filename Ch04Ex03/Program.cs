51153a3f7a33b4edb28b15c0a62eb45aaaa52025
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-08 15:53:24 +0200

    Ch04Ex03 Solution

--- Ch04Ex03/Program.cs
+++ Ch04Ex03/Program.cs
@@ -5,6 +5,34 @@ namespace Ch04Ex03
 {
     class Program
     {
-   
+        static void Main(string[] args)
+        {
+            // vars
+            const string myName = "benjamin";
+            const string niceName = "andrea";
+            const string sillyName = "ploppy";
+            string name;
+
+            // Ask for name
+            WriteLine("What is your name?");
+            name = ReadLine();
+
+            // Compare answered name with pre-defined names
+            switch (name.ToLower())
+            {
+                case myName:
+                    WriteLine("You have the same name as me!");
+                    break;
+                case niceName:
+                    WriteLine("My, what a nice name you have!");
+                    break;
+                case sillyName:
+                    WriteLine("That's a very silly name.");
+                    break;
+            }
+
+            // Greet
+            WriteLine($"Hello {name}!");
+        }
     }
 }
