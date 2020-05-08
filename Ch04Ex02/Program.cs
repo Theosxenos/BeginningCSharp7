7b3f63c1a730de086038ece7d279782973eb6857
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-08 15:42:00 +0200

    Ch04Ex02 init

---
+++ Ch04Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch04Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
