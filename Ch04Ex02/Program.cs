b7efb1218cff332b9f4fa3faa431cec24dcd14b6
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-08 15:48:19 +0200

    Ch04Ex02 Solution

--- Ch04Ex02/Program.cs
+++ Ch04Ex02/Program.cs
@@ -8,7 +8,28 @@ namespace Ch04Ex02
     {
         static void Main(string[] args)
         {
+            string comparison; 
             
+            // Ask for numbers
+            WriteLine("Enter a number:");
+            double var1 = ToDouble(ReadLine());
+            
+            WriteLine("Enter another number:");
+            double var2 = ToDouble(ReadLine());
+            
+            // Compared the 2 asked numbers
+            if (var1 < var2)
+                comparison = "less than";
+            else
+            {
+                if (var1 == var2)
+                    comparison = "equal to";
+                else
+                    comparison = "greater than";
+            }
+            
+            // Show result
+            WriteLine($"The first number is {comparison} the second number.");
         }
     }
 }
