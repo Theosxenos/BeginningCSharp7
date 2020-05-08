b8e364e5198af4ec53cf450d97893d327dd3ecce
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-08 16:09:04 +0200

    Ch04Ex05 Solution

--- Ch04Ex05/Program.cs
+++ Ch04Ex05/Program.cs
@@ -8,7 +8,35 @@ namespace Ch04Ex05
     {
         static void Main(string[] args)
         {
-            
+            // vars
+            double balance, interestRate, targetBalance;
+            int totalYears = 0;
+
+            // Ask for balance
+            WriteLine("What is your current balance?");
+            balance = ToDouble(ReadLine());
+
+            // Ask for interest rate
+            WriteLine("What is your current annual interest rate (in %)?");
+            interestRate = 1 + ToDouble(ReadLine()) / 100.0;
+
+            // Ask target balance
+            WriteLine("What balance would you like to have?");
+            targetBalance = ToDouble(ReadLine());
+
+            // Loop until the target balance is met or exceeded
+            // Addup the total years needed to meet the balance
+            while (balance < targetBalance)
+            {
+                balance *= interestRate;
+                ++totalYears;
+            }
+
+            // Output the result
+            WriteLine($"In {totalYears} year{(totalYears == 1 ? "" : "s")} you'll have a balance of {balance}.");
+
+            if (totalYears == 0)
+                WriteLine("To be honest, you really didn't need to use this calculator.");
         }
     }
 }
