dffc1b018c69529bbf2ee597ccd4e248a773b9c5
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-08 14:08:05 +0200

    Ch04Ex01 solution

--- Ch04Ex01/Program.cs
+++ Ch04Ex01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using static System.Convert;
 
 namespace Ch04Ex01
 {
@@ -7,7 +8,17 @@ namespace Ch04Ex01
     {
         static void Main(string[] args)
         {
-            
+            // Ask for a number and convert into int
+            WriteLine("Enter an integer:");
+            int myInt = ToInt32(ReadLine());
+            // Check the number for smaller than 10 or between 0 and 5
+            bool isLessThan10 = myInt < 10; 
+            bool isBetween0And5 = (0 <= myInt) && (myInt <= 5);
+            // Show the results
+            WriteLine($"Integer less than 10? {isLessThan10}");
+            WriteLine($"Integer between 0 and 5? {isBetween0And5}");
+            // Check for exclusive true
+            WriteLine($"Exactly one of the above is true? {isLessThan10 ^ isBetween0And5}");
         }
     }
 }
