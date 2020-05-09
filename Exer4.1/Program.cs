8d3ad820f59725397138b38e5cd9ff9749968900
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-09 13:37:24 +0200

    Exer4.1 solution

---
+++ Exer4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exer4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 11;
+            int var2 = 11;
+
+            Console.WriteLine($"{var1 > 10 ^ var2 > 10}");
+        }
+    }
+}
