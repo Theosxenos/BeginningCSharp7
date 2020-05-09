233e33024bad4d5bca98420245fa5b344c118675
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-09 13:45:32 +0200

    Exer 4.2 Solution

---
+++ Exer4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace Exer4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 0, var2 = 0;
+
+            do
+            {
+                WriteLine("Give me a number:");
+                var1 = ToInt32(ReadLine());
+
+                WriteLine("Give me another number:");
+                var2 = ToInt32(ReadLine());
+
+            } while (!(var1 > 10 ^ var2 > 10));
+
+            WriteLine($"{var1 > 10 ^ var2 > 10}");
+        }
+    }
+}
