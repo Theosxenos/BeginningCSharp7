700d318f9b1f49c8da038353e8a4a7f4896b89ec
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-12 20:07:05 +0200

    Ch05Ex04 Solution

---
+++ Ch05Ex04/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vars
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", "Shane Laigle" };
+            int i;
+
+            // Show the names of the friends
+            WriteLine($"Here are {friendNames.Length} of my friends:");
+            foreach (string friendName in friendNames)
+            {
+                WriteLine(friendName);
+            }
+        }
+    }
+}
