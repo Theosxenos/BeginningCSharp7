4ba47c355247b89d77f5d2ae9da3ed230ca56dd3
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-13 22:47:01 +0200

    Ch05Ex06 Solution

---
+++ Ch05Ex06/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Ch05Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "This is a test.";
+            char[] separator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(separator);
+
+            foreach (string word in myWords)
+            {
+                WriteLine($"{word}");
+            }
+        }
+    }
+}
