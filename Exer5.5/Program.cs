6a8c9461ba0eb506c0e65086f143660c11de8589
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-13 23:14:08 +0200

    Exer 5.5 Solution

---
+++ Exer5.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace Exer5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var
+            string userInput;
+            string editedString;
+
+            // Ask the user for a word
+            WriteLine("Please write a word:");
+            userInput = ReadLine();
+
+            // Replace all no, with yes
+            editedString = userInput.Replace("no", "yes");
+            WriteLine($"{editedString}");
+
+        }
+    }
+}
