447741fe813f9fb16dbc3d21c292a476fa5b8c95
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-13 23:25:00 +0200

    Exer 5.6 Solution

---
+++ Exer5.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Exer5._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vars
+            string userInput;
+            string[] userInputSplit;
+
+            // Ask user for a sentence:
+            WriteLine("Please write a sentence:");
+            userInput = ReadLine();
+
+            // Split the sentence on whitespaces, so it turns into an array with words
+            userInputSplit = userInput.Split();
+
+            WriteLine("Your sentence with quoted words:");
+            // Loop through all the words, append the double quotes, and a trailing whitespace
+            for (int i = 0; i < userInputSplit.Length; i++)
+            {
+                Console.Write($"\"{userInputSplit[i]}\" ");
+            }
+            WriteLine();
+        }
+    }
+}
