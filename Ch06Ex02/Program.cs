081ed913a22efc21aac6f72b5c2aabc3343ef97f
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-14 21:10:46 +0200

    Ch06Ex02&03 Solution

---
+++ Ch06Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+namespace Ch06Ex02
+{
+    class Program
+    {
+        /// <summary>
+        /// Gets the highest number from an array
+        /// </summary>
+        /// <param name="intArray">An array with numbers</param>
+        /// <returns>An integer of the highest number</returns>
+        static int MaxValue(int[] intArray)
+        {
+            int maxVal = intArray[0];
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                if (intArray[i] > maxVal)
+                    maxVal = intArray[i];
+            }
+            return maxVal;
+        }
+
+        static void Main(string[] args)
+        {
+            // var
+            int[] myArray = { 1, 8, 3, 6, 2, 5, 9, 3, 0, 2 };
+            // Call the function and show the result
+            int maxVal = MaxValue(myArray);
+            WriteLine($"The maximum value in myArray is {maxVal}");
+        }
+    }
+}
---
+++ Ch06Ex03/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace Ch06Ex03
+{
+    class Program
+    {
+        /// <summary>
+        /// Addup all the input numbers
+        /// </summary>
+        /// <param name="vals">The numbers to addup</param>
+        /// <returns>A sum of all the numbers</returns>
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            // Call the function and output the result
+            int sum = SumVals(1, 5, 2, 9, 8);
+            WriteLine($"Summed Values = {sum}");
+        }
+    }
+}
