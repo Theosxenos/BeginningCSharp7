97dd5af30f3ac50ea740c20545e812ed984bce8e
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-14 21:00:42 +0200

    Ch06Ex01 Solution

---
+++ Ch06Ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Ch06Ex01
+{
+    class Program
+    {
+        static void Write()
+        {
+            WriteLine("Text output from function.");
+        }
+
+        static void Main(string[] args)
+        {
+            Write();
+        }
+    }
+}
