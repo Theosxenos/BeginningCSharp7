bd49bd38e25c7005adc82423e9be6ce2aa6e6ce2
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-14 21:27:36 +0200

    Variable Scope: Using the Ch06Ex01

--- Ch06Ex01/Program.cs
+++ Ch06Ex01/Program.cs
@@ -4,14 +4,22 @@ namespace Ch06Ex01
 {
     class Program
     {
+        static string myString;
         static void Write()
         {
-            WriteLine("Text output from function.");
+            string myString = "String defined in Write()";
+            WriteLine("Now in Write()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
         }
-
         static void Main(string[] args)
         {
+            string myString = "String defined in Main()";
+            Program.myString = "Global string";
             Write();
+            WriteLine("\nNow in Main()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
         }
     }
 }
