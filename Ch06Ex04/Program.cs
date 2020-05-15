7390f2cbc396c18abbb4f3894343e409479aac0a
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-15 22:45:14 +0200

    Ch06Ex04 Solution

---
+++ Ch06Ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace Ch06Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"{args.Length} command line arguments were specified:");
+
+            foreach (string arg in args)
+                WriteLine(arg);
+        }
+    }
+}
