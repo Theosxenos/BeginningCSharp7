c1ed7adb65cf2213acb2e9fbcf62096b4c25bd69
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-17 10:00:51 +0200

    Exer6.2 Solution

---
+++ Exer6.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Exer6._2
+{
+    /// <summary>
+    /// Write an application that uses two command-line arguments to place values into a string and an integer variable, respectively.Then display those values.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First argument is string
+            string stringArg = "\"" + args[0] + "\"";
+            // Second is an int
+            int intArg = Convert.ToInt32(args[1]);
+
+            WriteLine($"The string argument is: {stringArg}, and the int argument is: {intArg}");
+        }
+    }
+}
