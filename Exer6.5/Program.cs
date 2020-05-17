a0c540233fb30de7745f4ac1ff8093c6292f040d
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-17 10:24:06 +0200

    Exer6.5 Solution

---
+++ Exer6.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exer6._5
+{
+    class Program
+    {
+        struct order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+            public double TotalCost() => unitCount * unitCost;
+
+            public string OrderInformation()
+            {
+                return $"Order Information: {unitCount} {itemName} items at ${unitCost} each, total cost ${TotalCost()}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            order MyOrder = new order() { itemName = "Book", unitCost=3.99, unitCount=5 };
+
+            Console.WriteLine(MyOrder.OrderInformation());
+        }
+    }
+}
