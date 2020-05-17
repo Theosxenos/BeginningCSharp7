13cb70915af34781b1e42be2ecf17638c0ed4673
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-17 10:11:14 +0200

    Exer6.3 Solution

---
+++ Exer6.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using static System.Console;
+
+namespace Exer6._3
+{
+    /// <summary>
+    /// Create a delegate and use it to impersonate the ReadLine() function when asking for user input.
+    /// </summary>
+    class Program
+    {
+        // Declare delegate
+        delegate string ReadLineDelegate();
+        // Declare fuction that calls and returns Console.Readline
+        static string MyReadLine() => ReadLine();
+
+        static void Main(string[] args)
+        {
+            // Assign function to delegate
+            ReadLineDelegate readline = new ReadLineDelegate(MyReadLine);
+            
+            // Ask for input and call delegate
+            WriteLine("Write something:");
+            string input = readline();
+
+            // Show the result
+            WriteLine($"You wrote: \"{input}\"");
+        }
+    }
+}
