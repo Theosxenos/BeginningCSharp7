aac09ef6a6787ebba803a8937a4d7cbf69ff3208
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-18 18:53:03 +0200

    Exer7.2 Solution

---
+++ Exer7.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exer7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5000; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 5000) throw new Exception("i == 5000");
+            }
+        }
+    }
+}
