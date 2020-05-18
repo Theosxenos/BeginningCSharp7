7f226da1c600de4a06a8741d4ecc36d8415f5e96
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-18 19:01:46 +0200

    Exer7.4 Solution

---
+++ Exer7.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace Exer7._4
+{
+    class Program
+    {
+        enum Orientation : byte
+        {
+            North = 1,
+            South = 2,
+            East = 3,
+            West = 4
+        }
+        static void Main(string[] args)
+        {
+            byte byteOrientationCorrect = 1;
+            int byteOrientationWrong = 335;
+            Orientation orientation;
+
+            try
+            {
+                WriteLine("Trying correct byte orientation");
+                Debug.WriteLine("Trying correct byte orientation");
+                orientation = checked((Orientation)(byteOrientationCorrect));
+
+                WriteLine("Trying wrong byte orientation");
+                Debug.WriteLine("Trying wrong byte orientation");
+                orientation = checked((Orientation)(byteOrientationWrong));
+            }
+            catch (OverflowException e)
+            {
+                Debug.WriteLine(e);
+                WriteLine("Failed succesfully!");
+            }
+
+        }
+    }
+}
