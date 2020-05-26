0568e1117f58e46b8a4f2594d9431fe6f128166b
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-05-26 14:54:39 +0200

    Ch10Ex01

---
+++ Ch10Ex01/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ch10Ex01
+{
+    public class MyClass
+    {
+        public readonly string Name;
+        private int intVal;
+        public int Val
+        {
+            get { return intVal; }
+            set
+            {
+                // Only values from 0 to 10 are allowed
+                if (value >= 0 && value <= 10)
+                    intVal = value;
+                else
+                    throw (new ArgumentOutOfRangeException("Val", value, "Val must be assigned a value between 0 and 10."));
+            }
+        }
+        public override string ToString() => "Name: " + Name + "\nVal: " + Val;
+        // Call the overloaded constructor at class/object creation
+        private MyClass() : this("Default Name") { }
+        // Because default constructor is private
+        public MyClass(string newName)
+        {
+            Name = newName;
+            intVal = 0;
+        }
+        private int myDoubledInt = 5;
+        public int myDoubledIntProp => (myDoubledInt * 2);
+    }
+}
---
+++ Ch10Ex01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch10Ex01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WriteLine("Creating object myObj...");
+            MyClass myObj = new MyClass("My Object");
+            WriteLine("myObj created.");
+            
+            // Try to assign the values -1, 0
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    WriteLine($"\nAttempting to assign {i} to myObj.Val...");
+                    myObj.Val = i;
+                    WriteLine($"Value {myObj.Val} assigned to myObj.Val.");
+                }
+                catch (Exception e)
+                {
+                    WriteLine($"Exception {e.GetType().FullName} thrown.");
+                    WriteLine($"Message:\n\"{e.Message}\"");
+                }
+            }
+            WriteLine("\nOutputting myObj.ToString()...");
+            WriteLine(myObj.ToString());
+            WriteLine("myObj.ToString() Output.");
+            WriteLine("\nmyDoubledIntProp = 5...");
+            WriteLine($"Getting myDoubledIntProp of 5 is {myObj.myDoubledIntProp}");
+        }
+    }
+}
