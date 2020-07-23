54dc88ba1550a1e9ef8f75e119006aa3d1780f3d
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2020-07-23 11:00:35 +0200

    Ch13Cardlib/Client

---
+++ Ch13CardClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ch13CardLib;
+using static System.Console;
+
+namespace Ch13CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck1 = new Deck();
+
+            try
+            {
+                Card myCard = deck1.GetCard(60);
+            }
+            catch (CardOutOfRangeException e)
+            {
+                WriteLine(e.Message);
+                WriteLine(e.DeckContents[0]);                
+            }
+        }
+    }
+}
---
+++ Ch13CardLib/CardOutOfRangeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ch13CardLib
+{
+    public class CardOutOfRangeException : Exception
+    {
+        public Cards DeckContents { get; }
+
+        /// <summary>
+        /// Constructor for the costum exception. Raises the base constructor with a default error message
+        /// </summary>
+        /// <param name="sourceDeckContents">Contents of the deck that raised the error</param>
+        public CardOutOfRangeException(Cards sourceDeckContents) : base("There are only 52 cards in the deck.")
+        {
+            DeckContents = sourceDeckContents;
+        }
+    }
+}
